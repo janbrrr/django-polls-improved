79e4fa8f12e123f83e253e8e9b9d6d20ac067b9b
Jan Barrera <42467335+janbrrr@users.noreply.github.com>
2019-07-13 11:12:35 +0200

    Formatted files via pre-commit hooks

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
@@ -9,14 +9,11 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
-    ]
+    fieldsets = [(None, {"fields": ["question_text"]}), ("Date information", {"fields": ["pub_date"]})]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
